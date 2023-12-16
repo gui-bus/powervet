f1b5b407382d78ea3982374778067a7f91babae7
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-12-16 17:03:48 -0300

    chore: add initial configs and images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+     colors: {
+       powerblue: {
+        dark: "#05538B",
+        light: "#2195E8"
+       }
+
+     }
     },
   },
   plugins: [],
